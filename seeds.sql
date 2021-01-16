03f0ed9d53f51e8c05c6b3daf6475c5c62e0b370
pjdip <pjdip1@gmail.com>
2021-01-16 12:31:06 -0500

    split seeds.sql into seeds and schema

--- seeds.sql
+++ seeds.sql
@@ -1,35 +1,3 @@
-/* Creating and using the database */
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db;
-
-/* Creating some tabes */
-CREATE TABLE departments (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE roles (
-    id INT AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(11,2) NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES departments(id)
-);
-
-CREATE TABLE employees (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
-);
-
 /* Adding stuff to departments */
 INSERT INTO departments (name)
 VALUES ("Sales");
